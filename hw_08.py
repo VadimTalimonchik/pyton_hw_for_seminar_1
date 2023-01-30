0f12e89e3e4495e272ca48b42febcb1473fb7ab1
VadimTalimonchik <tvadmins@gmail.com>
2023-01-30 19:11:55 +0300

    Задача 8

--- hw_08.py
+++ hw_08.py
@@ -9,5 +9,8 @@ n = int(input('Введите размер n: '))
 m = int(input('Введите размер m: '))
 k = int(input('Введите количество долек k: '))
 
-print(n ,'x', m)
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
 
